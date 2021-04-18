988957efa79dadc2372c715bba06f52b6097e3b2
DK <dk@dk-5570.home>
2021-04-17 19:45:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+price = input('10000')
+has_good_credit = true
+if has_good_credit
+ down_payment = 0.1 * "price"
+else:
+ down_payment = 0.2 * 'price'
+print("fDown Payment: {down_payment}")
+print('')
+print("FullNames:" + fullnames)
+print('Phone Number: '+ phone)
+print("Email: " + email)
+print("down_payment:" +str("down_payment"))
